75dc15b41a1472c289befd89b8530ffbff3c39a6
William Jackson <wcjaackson@gmail.com>
2025-05-06 22:46:35 +1000

    example other route

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template("index.html")
 
+@app.route("/api/get_data")
+def get_data():
+    return flask.jsonify({"message": "Hello, World!"})
+
 if __name__ == "__main__":
     app.run(debug=True)
